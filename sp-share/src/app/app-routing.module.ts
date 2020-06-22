dbeece086240ad2f30a1cb1863742ecc459ab108
ViacheslavBondarchuk <viacheslavbondarchuk@localhost.localdomain>
2020-06-22 23:38:27 +0300

    was added components main and login

--- sp-share/src/app/app-routing.module.ts
+++ sp-share/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+import {LoginComponent} from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
