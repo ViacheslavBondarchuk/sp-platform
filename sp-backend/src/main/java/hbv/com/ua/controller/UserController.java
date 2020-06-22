17e6382b87abc29a32f0a07b8133a0d90db7c7b1
ViacheslavBondarchuk <viacheslavbondarchuk@localhost.localdomain>
2020-06-23 00:29:56 +0300

    config

--- sp-backend/src/main/java/hbv/com/ua/controller/UserController.java
+++ sp-backend/src/main/java/hbv/com/ua/controller/UserController.java
@@ -1,11 +1,26 @@
 package hbv.com.ua.controller;
 
+import hbv.com.ua.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @GetMapping("/{id}")
+    public User read(@PathVariable long id) {
+        return new User();
+    }
+
+    @GetMapping
+    public List<User> readAll() {
+        return Collections.emptyList();
+    }
 
 }
---
+++ sp-backend/src/main/java/hbv/com/ua/model/User.java
@@ -0,0 +1,10 @@
+package hbv.com.ua.model;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+}
