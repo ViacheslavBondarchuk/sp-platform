526d5577a1657c574beececd1594add290967d8b
ViacheslavBondarchuk <viacheslavbondarchuk@localhost.localdomain>
2020-06-27 13:17:29 +0300

    was replaced all libs of projects

--- sp-backend/src/main/java/hbv/com/ua/controller/UserController.java
+++ sp-backend/src/main/java/hbv/com/ua/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,7 +20,7 @@ public class UserController {
 
     @GetMapping
     public List<User> readAll() {
-        return Collections.emptyList();
+        return Arrays.asList(new User());
     }
 
 }
--- sp-backend/src/main/java/hbv/com/ua/model/User.java
+++ sp-backend/src/main/java/hbv/com/ua/model/User.java
@@ -1,5 +1,12 @@
 package hbv.com.ua.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private long id;
     private String username;
