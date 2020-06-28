a2658a803d3ff6758c4e58f511300d2fe69b956c
ViacheslavBondarchuk <viacheslavbondarchuk@localhost.localdomain>
2020-06-28 15:11:24 +0300

    was added log4j2 logger and configuration, added util class for properties

---
+++ sp-backend/src/main/java/hbv/com/ua/util/PropertiesUtil.java
@@ -0,0 +1,46 @@
+package hbv.com.ua.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+public final class PropertiesUtil {
+    private static PropertiesUtil instance;
+    private Properties properties;
+
+    private final String PROPERTIES_NAME = "application.properties";
+
+    private PropertiesUtil() {
+    }
+
+    public static PropertiesUtil getInstance() {
+        if (instance == null) {
+            instance = new PropertiesUtil();
+        }
+        return instance;
+    }
+
+    private void init() {
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_NAME)) {
+            if (inputStream != null) {
+                this.properties = new Properties();
+                this.properties.load(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getProperty(final String key) {
+        return this.properties.getProperty(key);
+    }
+
+    public String getProperty(final String key, final String defaultValue) {
+        return this.properties.getProperty(key);
+    }
+
+}
